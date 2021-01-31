d8d8881cbbd451cd8e56515ed10cf8230fec8b0f
Urbain <urbain.lantres@tutanota.com>
2021-01-31 22:59:02 +0000

    Add trie_insert func

--- Cours/cours_1_5/Cours1Serie5.py
+++ Cours/cours_1_5/Cours1Serie5.py
@@ -1,28 +1,39 @@
 from random import randrange, shuffle
 
-t = [randrange(0,100) for i in range(50)]
+
+def trie_insert(liste):
+    for i in range(len(liste)):
+        tmp = liste[i]
+        j = i
+        while j > 0 and liste[j - 1] > tmp:
+            liste[j] = liste[j - 1]
+            j = j - 1
+        liste[j] = tmp
+    return liste
+
+
+t = [randrange(0, 100) for i in range(50)]
+
 
 def is_croissant(liste):
-	for i in range(len(liste)-1):
-		if liste[i] > liste[i+1]:
-			return False
+    for i in range(len(liste) - 1):
+        if liste[i] > liste[i + 1]:
+            return False
 
-	return True
+    return True
 
 
 def trie_singe(liste):
-	i=0
-	while not is_croissant(liste):
-		shuffle(liste)
-		print(i)
-		i+=1
-	return liste
-		
+    i = 0
+    while not is_croissant(liste):
+        shuffle(liste)
+        i += 1
+    return liste
 
-def trie_insert(liste):
-	pass
 
+def trie_postal(liste):
+    pass
 
 
 def main():
-	print(is_croissant(trie_singe()))
+    print(is_croissant(trie_insert(t)))
