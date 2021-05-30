b798fc969634a3875abed641ee4da8ce14752875
Urbain <urbain.lantres@tutanota.com>
2021-05-30 22:33:19 +0200

    Delete web/IMC directory

--- web/IMC/assets/js/script.js
+++
@@ -1,5 +0,0 @@
-function imc() {
-    var t = document.getElementById("taille").value
-    var m = document.getElementById("masse").value
-    document.getElementById('calc').innerHTML = `Votre IMC est : <i>${10000*m/(t*t)}</i>`
-}
